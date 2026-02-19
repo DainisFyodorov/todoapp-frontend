7492c4f75125777729bd918aa7cf495d55b6a4cd
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-19 09:41:03 +0200

    TaskModel implemented

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,15 @@
+class TaskModel {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+
+    constructor(id: number, title: string, description: string, completed: boolean) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+}
+
+export default TaskModel;
