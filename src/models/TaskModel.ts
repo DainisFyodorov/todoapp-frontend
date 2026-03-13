f9ccbcaed608544cfef72d9c6f86ce9e7b52d63e
DainisFyodorov <fedorovdainis@gmail.com>
2026-03-13 14:15:32 +0200

    Task priorities implemented

--- src/models/TaskModel.ts
+++ src/models/TaskModel.ts
@@ -3,13 +3,15 @@ class TaskModel {
     title: string;
     description: string;
     completed: boolean;
+    priority: string;
     categoryId: number | null;
 
-    constructor(id: number, title: string, description: string, completed: boolean, categoryId: number | null) {
+    constructor(id: number, title: string, description: string, completed: boolean, priority: string, categoryId: number | null) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.completed = completed;
+        this.priority = priority;
         this.categoryId = categoryId;
     }
 }
